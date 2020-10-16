29e0e882178dcc890cbe32bac178e5256767c8d7
Filipe Veronezi <filipeseidi@hotmail.com>
2020-10-16 02:08:34 -0300

    Day 2: finalizada landing page e página com mapa de orfanatos

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,26 @@
+// create map
+const mymap = L.map("mapid").setView([-23.1249006, -46.5567404], 15);
+
+// create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mymap);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+//create popup overlay
+const popup = L.popup({
+    closeButon: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+L
+.marker([-23.1249006, -46.5567404], { icon })
+.addTo(mymap)
+.bindPopup(popup)
